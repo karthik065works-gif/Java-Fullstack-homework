71c9eb29eb7d0bdb148dd9caaaf9a3c82afd4dd4
karthik065works-gif <karthik065works@gmail.com>
2026-09-13 15:43:24 +0530

    Create q3.java

---
+++ assign1/q3.java
@@ -0,0 +1,15 @@
+package Language_fundamentals.Home_work1.assign1;
+
+public class Q3 { 
+    public static void main(String[] args) {
+        String productName = "Backpack"; 
+        int price = 2000; 
+        int quantity = 2; 
+        boolean availability = true; 
+        System.out.println("Product"+productName); 
+        System.out.println("Price : " + price);  
+        System.out.println("Quantity:" + quantity);
+        System.out.println("availability:" + availability);
+    }
+    
+}
