70d325fe7f92d4aea4c21f0296bce3718ea2475d
karthik065works-gif <karthik065works@gmail.com>
2026-09-13 15:43:50 +0530

    Create q4.java

---
+++ assign1/q4.java
@@ -0,0 +1,17 @@
+package Language_fundamentals.Home_work1.assign1;
+
+public class Q4 { 
+    public static void main(String[] args) {
+        String employeeName = "Karthik"; 
+        int employeeId = 90119818; 
+        int salary = 2000000; 
+        String department = "SDG"; 
+        boolean permanentEmployeeStatus = false;
+        System.out.println("Employee:" + employeeName); 
+        System.out.println("Employee ID:" + employeeId ); 
+        System.out.println("Salary:" + salary); 
+        System.out.println("department:" + department); 
+        System.out.println("employment-status:"+permanentEmployeeStatus);
+    }
+    
+}
