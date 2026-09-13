7ca807eff7ff1fbe66ff8f9f69f4c50b5c0da492
karthik065works-gif <karthik065works@gmail.com>
2026-09-13 15:44:16 +0530

    Create q5.java

---
+++ assign1/q5.java
@@ -0,0 +1,17 @@
+package Language_fundamentals.Home_work1.assign1;
+
+public class Q5 { 
+    public static void main(String[] args) {
+        String brand = "SAMSUNG"; 
+        String model = "S26 ultra"; 
+        int price = 100000; 
+        String ram = "8GB"; 
+        boolean fivegStatus = true; 
+        System.out.println("Mobile-Brand:" + brand); 
+        System.out.println("Mobile model:" + model);  
+        System.out.println("Price:" + price);
+        System.out.println("RAM config : " + ram); 
+        System.out.println("5G status:" + fivegStatus); 
+    }
+    
+}
