dff9a3df2ad7fc01412c142b6a5111747205fc1b
karthik065works-gif <karthik065works@gmail.com>
2026-09-13 15:42:55 +0530

    Create q2.java

---
+++ assign1/q2.java
@@ -0,0 +1,18 @@
+package Language_fundamentals.Home_work1.assign1;
+
+public class Q2 {
+    public static void main(String[] args) {
+        String studentName = "KARTHIK"; 
+        int rollNumber = 106122065; 
+        int age = 23; 
+        int marks = 68; 
+        char grade = 'C'; 
+        boolean passStatus = true; 
+        System.out.println("studentName" + studentName); 
+        System.out.println("rollNumber" + rollNumber); 
+        System.out.println("age:" + age); 
+        System.out.println("marks :" + marks); 
+        System.out.println("grade:" + grade); 
+        System.out.println("Pass Status" + passStatus);
+    }
+}
