6a3365fc9467e67758a4ee157c8c33737dac5682
karthik065works-gif <karthik065works@gmail.com>
2026-09-15 09:23:51 +0530

    Create q3.java

---
+++ assign2/q3.java
@@ -0,0 +1,12 @@
+package Language_fundamentals.Home_work1.assign2;
+
+public class evenodd {
+    public static void  main(String[] args){
+        int num1 = 65;
+        if(num1%2 == 0){
+            System.out.println("The number is even:" + num1);
+        }else{
+            System.out.println("The number is odd:" + num1);
+        }
+    }
+}
