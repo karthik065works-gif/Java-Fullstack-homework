3cd507af3f279bdb1d0837d50e4d9c16b3def8be
karthik065works-gif <karthik065works@gmail.com>
2026-09-15 09:24:33 +0530

    Create q5.java

---
+++ assign2/q5.java
@@ -0,0 +1,13 @@
+package Language_fundamentals.Home_work1.assign2;
+
+public class largestof2nums {
+    public static void main(String[] args){
+        int num1 = 65;
+        int num2 = 101;
+        if(num1 > num2){
+            System.out.println("the number largest is :" + num1);
+        }else{
+            System.out.println("the number largest is :" + num2);
+        }
+    }
+}
