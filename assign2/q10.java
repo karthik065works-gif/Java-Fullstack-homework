d113de06c8ef4131760550966fd5e04221b24b5c
karthik065works-gif <karthik065works@gmail.com>
2026-09-15 09:26:09 +0530

    Create q10.java

---
+++ assign2/q10.java
@@ -0,0 +1,14 @@
+package Language_fundamentals.Home_work1.assign2;
+
+public class posnegzero {
+    public static void main(String[] args){
+        int num1 = 65;
+        if(num1 > 0){
+            System.out.println("The number enetered is positive:" + num1);
+        }else if(num1 < 0){
+            System.out.println("The number entered is negative" + num1);
+        }else{
+            System.out.println("the number is zero" + num1 );
+        }
+    }
+}
