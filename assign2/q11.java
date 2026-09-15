044f0ce16b46d2a3924d534f5f8e6f1cce8c726f
karthik065works-gif <karthik065works@gmail.com>
2026-09-15 09:26:26 +0530

    Create q11.java

---
+++ assign2/q11.java
@@ -0,0 +1,12 @@
+package Language_fundamentals.Home_work1.assign2;
+
+public class voteEligibility {
+    public static void main(String[] args){
+        int age = 23;
+        if(age >= 18){
+            System.out.println("Elligible to vote:");
+        }else{
+            System.out.println("Not elligible to vote");
+        }
+    }
+}
