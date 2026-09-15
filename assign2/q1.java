63df89af7924831d762615dd20dc537fa1c7390d
karthik065works-gif <karthik065works@gmail.com>
2026-09-15 09:23:09 +0530

    Create q1.java

---
+++ assign2/q1.java
@@ -0,0 +1,19 @@
+package Language_fundamentals.Home_work1.assign2;
+
+public class calculator {
+    public static void main(String[] args){
+        int num1 = 65;
+        int num2 = 47;
+        int add = num1 + num2;
+        int sub = num1 - num2;
+        int mul = num1*num2;
+        int div = num1/num2;
+        int mod = num1%num2;
+        System.out.println("-----------------------Basic calculator ops--------------------------");
+        System.out.println("the addition operation is:" + add);
+        System.out.println("The subtraction operation is:" + sub);
+        System.out.println("the multiplication is " + mul);
+        System.out.println("the division is" + div);
+        System.out.println("the modulo operation is " + mod);
+    }
+}
