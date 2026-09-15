5c6419af71663722cd3b7d669ca74175ef4ce033
karthik065works-gif <karthik065works@gmail.com>
2026-09-15 09:25:50 +0530

    Create q9.java

---
+++ assign2/q9.java
@@ -0,0 +1,12 @@
+package Language_fundamentals.Home_work1.assign2;
+
+public class passFail {
+    public static void main(String[] args){
+        int marks = 78;
+        if(marks >= 40){
+            System.out.println("the student has passed the exam with marks: " + marks);
+        }else{
+            System.out.println("The student failed the exam marks is :" + marks);
+        }
+    }
+}
